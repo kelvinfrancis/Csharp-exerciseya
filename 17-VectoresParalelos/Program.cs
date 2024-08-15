27dbed388b629e0eca2373fee20d2955a49986fc
kelvinfrancis <41239603+kelvinfrancis@users.noreply.github.com>
2024-08-15 17:20:30 -0400

    parte 17 competa, parte 18 - 1 ejercicio pendiente

---
+++ 17-VectoresParalelos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _17_VectoresParalelos
+{
+    internal class Program
+    {
+        class Problema1()
+        {
+            /*
+            Problema:
+            Desarrollar un programa que permita cargar 5 nombres de personas y sus edades respectivas. Luego de realizar la carga por teclado de todos los datos imprimir los nombres de las personas mayores de edad (mayores o iguales a 18 años)
+            */
+
+            string[] nombres;
+            int[] edades;
+
+            // Cargar datos
+            public void Cargar()
+            {
+                Console.Write("Ingrese la cantidad de personas: ");
+                string cantidad = Console.ReadLine();
+                int cant = int.Parse(cantidad);
+                nombres = new string[cant];
+                edades = new int[cant];
+
+                for (int i = 0; i < nombres.Length; i++)
+                {
+                    Console.Write("Ingrese un nombre: ");
+                    string linea = Console.ReadLine();
+                    nombres[i] = linea;
+
+                    Console.Write("Ingrese la edad: ");
+                    string edad = Console.ReadLine();
+                    edades[i] = int.Parse(edad);
+                }
+            }
+           
+            public void ImprimirMayores()
+            {
+                Console.WriteLine("Las personas mayores de edad son: ");
+                for (int i = 0;i < nombres.Length;i++)
+                {
+                    if (edades[i] >= 18)
+                        Console.WriteLine(nombres[i]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Problema1 pb1 = new Problema1();
+            pb1.Cargar();
+            pb1.ImprimirMayores();
+        }
+    }
+}
---
+++ 18-VectoresMayorYMenor/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace _18_VectoresMayorYMenor
+{
+    internal class Program
+    {
+        /* Problema 1:
+         Confeccionar un programa que permita cargar los nombres de 5 operarios y sus sueldos respectivos. Mostrar el sueldo mayor y el nombre del operario.
+         */
+
+        /* Problema 2:
+         Cargar un vector de n elementos. imprimir el menor y un mensaje si se repite dentro del vector
+         */
+        class Problema1()
+        {
+            string[] empleados = new string[5];
+            int[] sueldos = new int[5];
+
+            public void Cargar()
+            {
+                Console.WriteLine("Ingrese los nombres de los empreados: ");
+                for (int i = 0; i < empleados.Length; i++)
+                {
+                    Console.Write("Ingrese un nombre: ");
+                    string nombre = Console.ReadLine();
+                    empleados[i] = nombre;
+
+                    Console.Write("Ingrese su sueldo: ");
+                    string sueldo = Console.ReadLine();
+                    sueldos[i] = int.Parse(sueldo);
+                }
+            }
+
+            public void ImprimirSueldoMayor()
+            {
+                int mayor;
+                mayor = sueldos[0];
+                int indiceM = 0;
+                for (int i = 1;i < sueldos.Length; i++)
+                {
+                    if (sueldos[i] > mayor)
+                    {
+                        mayor = sueldos[i];
+                        indiceM = i;
+
+                    }
+                }
+                Console.WriteLine("Persona con el mayor sueldo es: {0}, con un sueldo de {1}", empleados[indiceM], mayor);
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            Problema1 pb1 = new Problema1();
+            pb1.Cargar();
+            pb1.ImprimirSueldoMayor();
+        }
+    }
+}
